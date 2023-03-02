478cc3863a9e090bfa90939f72d31b1fad5dfc4c
zikriaulia28 <zikriaulia98@gmail.com>
2023-03-03 01:35:23 +0700

    create get request

--- index.js
+++ index.js
@@ -4,7 +4,21 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+// URL = PROTOCOL://HOST:PORT/ENDPOINT 
+// PROTOCOL = http, https
+// HOST = ip, domain
+// PORT = ketika ip ada port
+// ENDPOINT = alat navigasi
+
+app.get("/", (req, res) => {
+  // res.json({
+  //   msg: "Selamat Datang di Toko Kopi API",
+  // })
+  const path = require('path');
+  res.status(200).sendFile(path.join(__dirname, "/src/html/wellcome.html"))
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`)
-  console.log("wellcome")
+  // console.log("wellcome")
 })
