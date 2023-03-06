97fd47448e1c8769324387ce8e2ed595f9577286
zikriaulia28 <zikriaulia98@gmail.com>
2023-03-06 23:33:02 +0700

    update config pg

--- src/configs/environtment.js
+++ src/configs/environtment.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-undef */
 module.exports = {
-  host: process.env.HOST,
-  db: process.env.DB,
+  host: process.env.DB_HOST,
+  db: process.env.DB_NAME,
   dbPort: process.env.DB_PORT,
-  user: process.env.UNAME,
+  user: process.env.DB_USER,
   pass: process.env.DB_PASS,
   serverPort: process.env.SERVER_PORT,
 };
