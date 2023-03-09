4a68cfb4406206ee477008ee7de373cbbeddc01e
zikriaulia28 <zikriaulia98@gmail.com>
2023-03-10 01:31:45 +0700

    update auth

--- src/models/auth.model.js
+++ src/models/auth.model.js
@@ -2,7 +2,8 @@ const db = require("../configs/postgre");
 
 const userVerification = (body) => {
   return new Promise((resolve, reject) => {
-    const sql = "SELECT u.id, u.email, pr.display_name from users u JOIN profile pr ON pr.user_id = u.id WHERE email = $1 and password = $2";
+    // const sql = "SELECT u.id, u.email, pr.display_name from users u JOIN profile pr ON pr.user_id = u.id WHERE email = $1 and  password = $2";
+    const sql = "SELECT id, email, phone_number from users WHERE email = $1 and password = $2";
     const values = [body.email, body.password];
     db.query(sql, values, (err, result) => {
       if (err) {
