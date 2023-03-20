4bc4d2f4a5bbe4ec6c897720fb2b24082faf9575
zikriaulia28 <zikriaulia98@gmail.com>
2023-03-20 16:49:36 +0700

    add cloudinary

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -73,8 +73,7 @@ const insertProducts = async (req, res) => {
 const cloudUpload = async (req, res) => {
   try {
     // upload ke cloud
-    const { params } = req;
-    const { data, err, msg } = await uploader(req, "Welcome", params);
+    const { data, err, msg } = await uploader(req);
     if (err) throw { msg, Error };
     if (!data) return res.status(200).json({ msg: "No File Uploaded" });
     res.status(201).json({ data, msg });
