748f26315f37d8e94baea1c12fcb125c53870a11
zikriaulia28 <zikriaulia98@gmail.com>
2023-03-23 23:41:39 +0700

    update limit upload image

--- src/middlewares/memoryUpload.js
+++ src/middlewares/memoryUpload.js
@@ -3,13 +3,13 @@ const path = require("path");
 
 const storage = multer.memoryStorage();
 
-const limits = 10 * 1024 * 1024;// 10 MB
+const limits = 2 * 1024 * 1024; // 2 MB
 
 const fileFilter = (req, file, cb) => {
-  const pattern = /jpg|png|webp/i;
+  const pattern = /png|jpg|jpeg|webp/i;
   const ext = path.extname(file.originalname);
   if (!pattern.test(ext)) {
-    const error = new Error("Tipe file tidak valid. Hanya file JPG, PNG, dan WEBP yang diizinkan.");
+    const error = new Error("Tipe file tidak valid. Hanya file JPG, PNG, JPEG dan WEBP yang diizinkan.");
     error.status = 400; // Tambahkan kode status pada objek error
     return cb(error, false);
   }
--- src/routers/users.route.js
+++ src/routers/users.route.js
@@ -1,15 +1,16 @@
 const { Router } = require("express");
 const usersController = require("../controllers/users.controller");
 const usersRouter = Router();
+const { checkRole } = require("../controllers/auth.controller");
 
 // Create
 usersRouter.post("/", usersController.insertUsers);
 // Read
 usersRouter.get("/", usersController.getUsers);
-usersRouter.get("/:id", usersController.getUserDetail);
+usersRouter.get("/:id", checkRole, usersController.getUserDetail);
 // Update
-usersRouter.patch("/:id", usersController.updateUsers);
+usersRouter.patch("/:id", checkRole, usersController.updateUsers);
 // Delete
-usersRouter.delete("/:id", usersController.deleteUsers);
+usersRouter.delete("/:id", checkRole, usersController.deleteUsers);
 
 module.exports = usersRouter;
