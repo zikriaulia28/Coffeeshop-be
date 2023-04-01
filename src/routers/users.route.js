e312c6d11d8e9347b018920019b5adfca95fcc09
zikriaulia28 <zikriaulia98@gmail.com>
2023-04-02 03:07:35 +0700

    add check token user

--- src/routers/users.route.js
+++ src/routers/users.route.js
@@ -2,15 +2,16 @@ const { Router } = require("express");
 const usersController = require("../controllers/users.controller");
 const usersRouter = Router();
 const { checkRole } = require("../controllers/auth.controller");
+const { checkToken } = require("../middlewares/auth");
 const memoryUpload = require("../middlewares/memoryUpload");
 
 // Create
 usersRouter.post("/", usersController.insertUsers);
 // Read
 usersRouter.get("/", usersController.getUsers);
-usersRouter.get("/:id", usersController.getUserDetail);
+usersRouter.get("/:id", checkToken, usersController.getUserDetail);
 // Update
-usersRouter.patch("/:id", memoryUpload.single("image"), usersController.updateUsers);
+usersRouter.patch("/:id", checkToken, memoryUpload.single("image"), usersController.updateUsers);
 // Delete
 usersRouter.delete("/:id", checkRole, usersController.deleteUsers);
 
